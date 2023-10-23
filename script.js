cea344688866b71fc4785993547bc12ac121a14c
mittelmanjournal <147964412+mittelmanjournal@users.noreply.github.com>
2023-10-22 20:01:01 -0700

    Add files via upload
    Changed script.js so that instead of it changing the color of the header text when showing full body text, it simply hides the header text and the body text contains the words in the header so it has the same effect.
    
    This now means on any width device the text will naturally fill each line, before this change the end of the header text meant that the body text started where the header line ends.

--- script.js
+++ script.js
@@ -5,25 +5,25 @@ function toggleDiv(post, gradient, btn) {
     if (postBody.style.display != "none") {
         showHide.textContent = "Show";
         postBody.style.display = "none";
+        postHeader.style.display = "inline-block";
         postHeader.style.color = "transparent";
         postHeader.style.backgroundImage = "linear-gradient(to left, white, black)";
     } else {
         showHide.textContent = "Hide";
         postBody.style.display = "inline-block"; // Change to inline-block to appear on the same line
-        postHeader.style.color = "black";
-        postHeader.style.backgroundImage = "none";
+        postHeader.style.display = "none";
     }
 }
 
-// JavaScript code
-window.addEventListener('DOMContentLoaded', function() {
-    var gradientTextDesktop = document.getElementById('gradient-text-1-desktop');
-    var gradientTextMobile = document.getElementById('gradient-text-1-mobile');
+// // JavaScript code
+// window.addEventListener('DOMContentLoaded', function() {
+//     var gradientTextDesktop = document.getElementById('gradient-text-1-desktop');
+//     var gradientTextMobile = document.getElementById('gradient-text-1-mobile');
 
-    // Check if the screen width is less than or equal to 767px (typical for mobile devices)
-    if (window.innerWidth <= 767) {
-        gradientTextMobile.style.display = 'inline'; // Show mobile version text
-    } else {
-        gradientTextDesktop.style.display = 'inline'; // Show desktop version text
-    }
-});
+//     // Check if the screen width is less than or equal to 767px (typical for mobile devices)
+//     if (window.innerWidth <= 767) {
+//         gradientTextMobile.style.display = 'inline'; // Show mobile version text
+//     } else {
+//         gradientTextDesktop.style.display = 'inline'; // Show desktop version text
+//     }
+// });
